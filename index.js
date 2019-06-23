070f52f59b654557d37ab833e1f3a46b974f2986
Johnson Mabgwe <mabgwej@gmail.com>
2019-06-23 22:25:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ function whisper(string) {
   return string.toLowerCase()
 }
  function logShout(string) {
-  (lowercase.toLowerCase() ===lowercase)
-  return string()
+  return string.toUpperCase
 }
